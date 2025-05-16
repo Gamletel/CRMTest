dcaeb0b4a6cc46e82e6d54b1cca7c9ce1c1f542f
Gamletel <gamletel@list.ru>
2025-05-16 16:51:08 +0500

    Added docker, auth for users

--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,24 @@
 <?php
 
+use App\Http\Controllers\Api\v1\AuthController;
+use App\Http\Controllers\Api\v1\LoginController;
 use App\Http\Controllers\Api\v1\UserController;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
 Route::prefix('v1')
-    ->middleware('api')
     ->group(function () {
-        Route::middleware('auth:sanctum')->group(function () {
-            Route::apiResource('user', UserController::class)->only(['index', 'show', 'store', 'update', 'destroy']);
-        });
+        Route::apiResource('user', UserController::class)
+            ->only(['index', 'show', 'store', 'update', 'destroy'])
+            ->middleware('auth:sanctum');
+
+        Route::post('/tokens/test', [AuthController::class, 'createTestToken']);
     });
+
+Route::post('/login', [LoginController::class, 'login'])->name('login');
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+Route::post('/register', [AuthController::class, 'register'])->name('register');
